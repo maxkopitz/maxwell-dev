39c6c529a7fdff9313ea743625ca2c3bf4b51029
Max Kopitz <mlkopitz@umich.edu>
2022-11-10 21:24:07 -0500

    component(footer): Added desired footer

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function footer() {
+  return (
+    <footer className="text-center mt-12 mb-4">
+      Made with ♥️  in <Link href="/">Ann Arbor</Link>
+    </footer>
+  )
+};
