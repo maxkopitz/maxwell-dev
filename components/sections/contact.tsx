862adcc870bc231c0f8e0e443722444f445b3d42
Max Kopitz <mlkopitz@umich.edu>
2022-11-11 16:22:20 -0500

    feat(contact): layout

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -1,7 +1,18 @@
-export default function Contact() {
+const Contact : React.FC = () => {
   return (
-    <div className="h-min">
-      <h1>contact</h1>
+    <div id="contact" className="h-min grid place-items-center content-center p-4">
+      <div className="max-w-8xl w-full flex flex-wrap justify-center">
+        <h2 className='text-4xl font-bold text-center w-full'>
+          Whats next?:
+        </h2>
+        <div className="flex items-center justify-center">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline" type="submit">
+            Say hi
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
+
+export default Contact;
