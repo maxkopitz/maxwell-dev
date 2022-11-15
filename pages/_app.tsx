fc9dca53f046363b3967205c794700ac1ca8f7bc
Max Kopitz <mlkopitz@umich.edu>
2022-11-15 16:14:47 -0500

    Big

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
+import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+      <Analytics />
+    </ThemeProvider>)
 }
