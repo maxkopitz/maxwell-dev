4f6c302a6bae0de749341e6b3c1438907552747b
Max Kopitz <mlkopitz@umich.edu>
2022-11-14 20:12:25 -0500

    fix(section): Divide not working on mobile

--- components/sections/experience.tsx
+++ components/sections/experience.tsx
@@ -3,7 +3,7 @@ export default function Experience() {
     <div className="h-screen">
       <div className="container rounded-md h-3/6 w-4/6 mx-auto">
         <div className="p-10">
-          <h3 className="text-2xl text-yellow-400">02. <span className="text-3xl text-orange-500">Experience</span> &#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;</h3>
+          <h3 className="text-2xl text-yellow-400">02. <span className="text-3xl text-orange-500">Experience</span> &#9473;</h3>
         </div>
       </div>
     </div>
