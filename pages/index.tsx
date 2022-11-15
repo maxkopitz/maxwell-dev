2fef157bdd3ee27099c358688279675060b88115
Max Kopitz <mlkopitz@umich.edu>
2022-11-14 19:50:19 -0500

    feat(sidebar): Started to implement

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,11 @@ import Projects from '../components/sections/projects'
 import Contact from '../components/sections/contact'
 import Footer from '../components/footer'
 
-const Home : React.FC = () => {
+/* NOTE: About and sidebars inspired from https://brittanychiang.com/
+ * TODO: When complete give credit in README.md
+ */
+
+const Home: React.FC = () => {
   return (
     <div>
       <Head>
@@ -15,10 +19,28 @@ const Home : React.FC = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <About/>
-        <Experience />
-        <Projects />
-        <Contact />
+        <div className="flex flex-row">
+          <div className="w-16 h-screen text-white sticky top-0">
+            <div className="absolute text-center inset-x-0 bottom-52">
+              <h1>G</h1>
+              <h1>L</h1>
+              <h1>I</h1>
+              <h1>T</h1>
+            </div>
+          </div>
+          <div className="grow">
+            <About />
+            <Experience />
+            <Projects />
+            <Contact />
+          </div>
+          <div className="w-16 h-screen text-white sticky top-0">
+            <div className="absolute text-center inset-x-0 bottom-64 rotate-90">
+              <h1>maxkopitz02@gmail.com</h1>
+            </div>
+          </div>
+        </div>
+
       </main>
       <Footer />
     </div>
