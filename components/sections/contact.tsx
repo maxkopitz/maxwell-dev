51973e3d06e2d644aac58e56d7581c178373b785
Max Kopitz <mlkopitz@umich.edu>
2022-11-14 19:12:56 -0500

    feat(sections): Headings

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -1,16 +1,19 @@
-const Contact : React.FC = () => {
+const Contact: React.FC = () => {
   return (
-    <div id="contact" className="h-min grid place-items-center content-center p-4">
-      <div className="max-w-8xl w-full flex flex-wrap justify-center">
-        <h2 className='text-4xl font-bold text-center w-full'>
-          Whats next?:
+    <div className="h-screen">
+      <div className="max-w-8xl w-full">
+      <div className="text-center">
+        <h2 className='text-4xl text-sky-100 font-bold w-full'>
+          Whats next?
         </h2>
-        <div className="flex items-center justify-center">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline" type="submit">
-            Say hi
-          </button>
-        </div>
       </div>
+      <div className="flex flex-col items-center mt-5">
+       <button className="bg-green-500 hover:bg-green-700 text-white text-xl font-bold py-2 px-12 rounded focus:outline-none focus:shadow-outline" type="submit">
+          Say hi
+        </button>
+ 
+      </div>
+     </div>
     </div>
   )
 }
--- components/sections/experience.tsx
+++ components/sections/experience.tsx
@@ -1,7 +1,11 @@
 export default function Experience() {
   return (
     <div className="h-screen">
-      <h1>Experience</h1>
+      <div className="container rounded-md h-3/6 w-4/6 mx-auto">
+        <div className="p-10">
+          <h3 className="text-2xl text-yellow-400">02. <span className="text-3xl text-orange-500">Experience</span> &#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;&#9473;</h3>
+        </div>
+      </div>
     </div>
   )
 }
