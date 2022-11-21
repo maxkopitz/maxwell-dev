b2994b970e80f16d225b142a86b5c8da59669d3e
Max Kopitz <mlkopitz@umich.edu>
2022-11-21 17:00:32 -0500

    GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,26 +2,12 @@ import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
-import Script from "next/script";
 
 /* NOTE: https://nextjs.org/docs/messages/next-script-for-ga */
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-      />
-      <Script id="google-analytics-script" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          page_path: window.location.pathname,
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-          });
-    `}
-      </Script>
+     
       <Component {...pageProps} />
       <Analytics />
     </ThemeProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,22 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+        <Script id="google-analytics-script" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}')
+    `}
+        </Script>
         <link href="/site.webmanifest" rel="manifest" />
         <meta content="#ffffff" name="theme-color" />
         <meta
