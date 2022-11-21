27e014279705eb76b1b735a96fbc561a0e042512
Max Kopitz <mlkopitz@umich.edu>
2022-11-21 15:01:42 -0500

    Added note

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,23 +4,32 @@ import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import Script from "next/script";
 
+/* NOTE: https://nextjs.org/docs/messages/next-script-for-ga */
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Script
-        strategy="lazyOnload"
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
       />
-      <Script id="google-analytics-script" strategy="lazyOnload">
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+        `}
+      </Script>
+      {/*<Script id="google-analytics-script" strategy="lazyOnload">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
           page_path: window.location.pathname,
           });
     `}
-      </Script>
+      </Script> */}
       <Component {...pageProps} />
       <Analytics />
     </ThemeProvider>
