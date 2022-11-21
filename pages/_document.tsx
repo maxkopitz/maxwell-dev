8356518b8b667c743e10485661b54ebd420c6249
Max Kopitz <mlkopitz@umich.edu>
2022-11-20 23:02:51 -0500

    feat(google-site-verification)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta content="#ffffff" name="theme-color" />
         <link href="/site.webmanifest" rel="manifest" />
+        <meta content="#ffffff" name="theme-color" />
+        <meta name="google-site-verification" content="Gu06v3QxgyAEa-Of4Y4h8mWzgCoTZTYWv1tZDuPofP4" />
       </Head>
       <body className="bg-white dark:bg-black text-white dark:text-black">
         <Main />
