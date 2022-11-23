bf40c6978f9c18c6b1a353bb5993b291d6cec23a
Max Kopitz <mlkopitz@umich.edu>
2022-11-23 00:07:26 -0500

    fix(NowPlaying): Colors in light
    Changed to green and made same color for light and dark

--- components/features/NowPlaying.tsx
+++ components/features/NowPlaying.tsx
@@ -61,15 +61,15 @@ function AnimatedBars() {
     <div className="w-auto flex items-end overflow-hidden">
       <span
         id="bar1"
-        className="w-1 mr-[3px] h-2 bg-gray-300 dark:bg-gray-500 opacity-75"
+        className="w-1 mr-[3px] h-2 bg-green-500 opacity-75"
       />
       <span
         id="bar2"
-        className="w-1 mr-[3px] h-1 bg-gray-300 dark:bg-gray-500"
+        className="w-1 mr-[3px] h-1 bg-green-500"
       />
       <span
         id="bar3"
-        className="w-1 h-3 bg-gray-300 dark:bg-gray-500 opacity-80"
+        className="w-1 h-3 bg-green-500 opacity-80"
       />
     </div>
   );
