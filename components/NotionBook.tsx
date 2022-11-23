9a15845f7356efc47112534ba55bc59e86d188b1
Max Kopitz <mlkopitz@umich.edu>
2022-11-23 00:08:59 -0500

    feat(NotionBook): date
    Added time it took me to read

--- components/NotionBook.tsx
+++ components/NotionBook.tsx
@@ -1,32 +1,42 @@
 import { NextPage } from "next";
 import { NotionReadingItem } from "lib/types";
 
+
+/* TODO: I want to add more information
+ */
 const NotionBook: NextPage<{ item: NotionReadingItem }> = ({ item }) => {
   let badge;
+  let date;
   switch (item.data.status) {
     case "COMPLETED":
       badge = (
-        <span className="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-400 dark:text-green-800">
+        <span className="bg-green-800 text-green-100 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
           Finished
         </span>
       );
+      const start = new Date(item.data.start_date);
+      const end = new Date(item.data.end_date);
+      date = {
+        timeToRead: Math.abs(end.getTime() - start.getTime()) / (1000 * 3600 * 24),
+        start,
+        end,
+      }
       break;
     case "READING":
       badge = (
-        <span className="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-400 dark:text-red-800">
+        <span className="bg-red-800 text-red-100 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
           Reading
         </span>
       );
       break;
     case "Not Started":
       badge = (
-        <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-400 dark:text-blue-800">
+        <span className="bg-blue-800 text-blue-100 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
           Not started
         </span>
       );
       break;
   }
-
   return (
     <div className="bg-white dark:bg-gray-900 border mb-4 border-gray-200 dark:border-gray-800 rounded-lg p-4 max-w-72 w-full">
       <div className="">
@@ -35,9 +45,11 @@ const NotionBook: NextPage<{ item: NotionReadingItem }> = ({ item }) => {
           {item.data.name}
         </h1>
       </div>
-      <h1 className="text-black dark:text-white">
-        {item.data.author} - {item.data?.start_date} to {item.data?.end_date}
-      </h1>
+      <h2 className="text-black dark:text-white">
+        {item.data.author}
+      </h2>
+      {date && (<div className="text-gray-800 dark:text-gray-400"><p>Took me {date.timeToRead} days to read</p> 
+        <p>{date.start.toDateString()} to {date.end.toDateString()}</p></div>)}
     </div>
   );
 };
