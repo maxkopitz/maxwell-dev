a53373b7f99416141bf138306a3283ffb09c3966
Max Kopitz <mlkopitz@umich.edu>
2022-11-27 23:38:42 -0500

    feat(about): more progress

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,7 @@ import Container from "components/ui/Container";
 import { NextPage } from "next";
 
 const About: NextPage = () => {
-  const skills = ['JavaScript', 'C++ (11 & 17)', 'React', 'NextJS', 'Node.js', 'Python'];
+  const skills = ['JavaScript', 'C++ (11 & 17)', 'React', 'NextJS', 'Node.js', 'Python', 'Postgres', 'AWS'];
   return (
     <Container title="Maxwell Kopitz - About" description="My about page">
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
@@ -11,9 +11,10 @@ const About: NextPage = () => {
         </h1>
         <p>Hi! I&apos;m max. I love building. My journey started back in 2014 when I learned Java to create
           Minecraft plugins.</p>
-        <ul className="grid grid-cols-2">
-          {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
-        </ul>
+          <ul className="grid grid-cols-2 text-yellow-400">
+            {skills && skills.map((skill, i) => <li key={i}>▹{skill}</li>)}
+          </ul>
+
       </div>
     </Container>
   );
