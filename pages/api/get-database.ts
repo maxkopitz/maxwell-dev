9a7307bf22fc2ec67929d49b68d597c1bb2bae11
Max Kopitz <mlkopitz@umich.edu>
2023-01-05 15:18:35 -0500

    Added get-database without edge

---
+++ pages/api/get-database.ts
@@ -0,0 +1,12 @@
+import { getPosts } from "lib/notion";
+
+export default async function handler(req, res) {
+  const response = await getPosts();
+
+  const scheme = response.scheme;
+  const items = response.items;
+  res.status(200).json({
+    scheme,
+    items,
+  })
+}
