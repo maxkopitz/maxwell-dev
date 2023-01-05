8998e381c7804eb0a7ed158fa1a912771d3d50ed
Max Kopitz <mlkopitz@umich.edu>
2023-01-05 14:28:24 -0500

    Notion

--- pages/api/get-notion.ts
+++ pages/api/get-notion.ts
@@ -6,21 +6,36 @@ export const config = {
 };
 
 export default async function handler(req: NextRequest) {
-  const response = await getPosts();
+  try {
+    const response = await getPosts();
 
-  const scheme = response.scheme;
-  const items = response.items;
-  return new Response(
-    JSON.stringify({
-      scheme,
-      items,
-    }),
-    {
-      status: 200,
-      headers: {
-        "content-type": "application/json",
-        "cache-control": "public, s-maxage=60, stale-while-revalidate=30",
-      },
-    }
-  );
+    const scheme = response.scheme;
+    const items = response.items;
+    return new Response(
+      JSON.stringify({
+        scheme,
+        items,
+      }),
+      {
+        status: 200,
+        headers: {
+          "content-type": "application/json",
+          "cache-control": "public, s-maxage=60, stale-while-revalidate=30",
+        },
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error: error
+      }),
+      {
+        status: 200,
+        headers: {
+          "content-type": "application/json",
+          "cache-control": "public, s-maxage=60, stale-while-revalidate=30",
+        },
+      }
+    );
+  }
 }
