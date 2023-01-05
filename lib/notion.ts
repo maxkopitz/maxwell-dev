3630fc717ba6a46e3a7dc2eb34355ef712b821f0
Max Kopitz <mlkopitz@umich.edu>
2023-01-05 17:30:05 -0500

    Sorting by descending date

--- lib/notion.ts
+++ lib/notion.ts
@@ -5,17 +5,17 @@ const notion = new Client({
 });
 
 const processDatabase = (database) => {
-  const notionDatabase = { scheme: "test", items: [] };
+  const notionDatabase = { scheme: "reading", items: [] };
   database.results.forEach((result) => {
     const properties = result.properties;
     const row = {
       id: result.id,
       data: {
-        name: properties.Name.title[0].plain_text,
-        author: properties.Author.rich_text[0].plain_text,
-        status: properties.Status.multi_select[0].name,
-        start_date: properties.Start.date?.start,
-        end_date: properties.End.date?.start,
+        name: properties?.Name.title[0].plain_text,
+        author: properties?.Author.rich_text[0].plain_text,
+        status: properties?.Status.multi_select[0].name,
+        start_date: properties?.Start.date?.start,
+        end_date: properties?.End.date?.start,
       },
     };
     notionDatabase.items.push(row);
@@ -30,8 +30,11 @@ export async function getPosts() {
         property: "Status",
         direction: "ascending",
       },
+      {
+        property: "Start",
+        direction: "descending",
+      }
     ],
   });
-  console.log(response);
   return processDatabase(response);
 }
